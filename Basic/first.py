4771dc9508e86901a8123eaf59a8ee3dcdc9a307
VIVEK KUMAR <121549772+Vivekkumar121@users.noreply.github.com>
2023-04-08 20:19:50 +0530

    Add files via upload

---
+++ Basic/first.py
@@ -0,0 +1,7 @@
+""" hi this is my first day, i am very exciting to do this cause this is first time i am watching many projects inside any language and so i am learning this language"""
+#Author : Vivek
+#course : 100DaysChallange'
+print("HELLO\nWORLD, 7",8, sep="~", end=" 09\n"), print(5) 
+# ncdjncd
+print("hey this is my first program \'and i am learning it \'from code with harry")
+print(14*32)

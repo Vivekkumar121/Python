1a91911bdd5dbaea9a2bf06eabfb36784f98c8f7
VIVEK KUMAR <121549772+Vivekkumar121@users.noreply.github.com>
2023-04-08 20:20:21 +0530

    Add files via upload

---
+++ Basic/sixth.py
@@ -0,0 +1,16 @@
+a=2
+b="vivek kumar"
+c=None
+d=True
+q=complex(2,3)
+print(q)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+list={1,2.3,"tuoid",3}
+print(list)
+tuple=(23,34.44,"vivek","himahnshu")
+print(tuple)
+dict={"name":"vivek"}
+print(dict)

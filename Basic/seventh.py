ee29d1c4f1c4d53abeb5cba1bf45c6ea6daa85ed
VIVEK KUMAR <121549772+Vivekkumar121@users.noreply.github.com>
2023-04-10 23:51:54 +0530

    Add files via upload

---
+++ Basic/seventh.py
@@ -0,0 +1,17 @@
+"""print(42%8)
+print(42/8)
+print(42+8)
+print(3**2)
+
+input(b)
+print(a+b)
+"""
+a=40
+b=20
+print("the value of addition is : ",a+b)
+print("the value of subtraction is : ",a-b)
+print("the value of multiplication is : ",a*b)
+print("the value of division is : ",a%b)
+
+
+

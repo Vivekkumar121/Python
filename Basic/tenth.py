4f05cb67d932650e2bb9a99d5dff3735d1da8c50
VIVEK KUMAR <121549772+Vivekkumar121@users.noreply.github.com>
2023-04-11 07:45:24 +0530

    Add files via upload

---
+++ Basic/tenth.py
@@ -0,0 +1,8 @@
+a=input("Enter your name : ")
+b= input("Enter your second name : ")
+print("your name is ",a,b)
+
+x=input("Eenter number 1 ")
+y=input("Eenter number 2 ")
+print("concatenation of x and y is : ",a+b)
+print("sum of x and y is : ",int(x)+int(y))

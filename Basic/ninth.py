4974a6fbec3abece4caaf5428876dbb2ad62067a
VIVEK KUMAR <121549772+Vivekkumar121@users.noreply.github.com>
2023-04-12 23:11:14 +0530

    Add files via upload

---
+++ Basic/ninth.py
@@ -0,0 +1,14 @@
+a="2"
+b="4"
+print(a+b)
+print(int(a) + int(b))
+z="v"
+print()
+
+# Type casting : the conversion of one datatype to another
+"""implicit type casting : isme jo datatype h compiler ya interpreter khud se convert krta h ek datat type ko dusre datatype mein 
+for e.g
+a=10   this is integer data type
+b=2.5 this is float
+print(a+b) it will be float so dattype b is converted ino float automatically by the interpreter 
+Explicit type casting : opposite of implicit means we convert one data type to another """

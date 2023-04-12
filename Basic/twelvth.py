4f7e207c82c61ecd038209a08a349ddab0373083
VIVEK KUMAR <121549772+Vivekkumar121@users.noreply.github.com>
2023-04-12 23:11:54 +0530

    Add files via upload

---
+++ Basic/twelvth.py
@@ -0,0 +1,41 @@
+#video 12
+# names="vivek kuamr rajput"
+# print(len(names))
+# print(names[4:4])
+# print(names[:])
+# print(names[-11:-1])
+
+# len1="I am a student of kiet group of student"
+# len2=len(len1)
+# print(len2)
+#  #Quiz
+# nm="Vivek"
+# print(nm[-4:-2])
+
+# video 13 string methods
+a="viEvek"
+print(a.upper())
+print(a.lower())
+print(a)
+b="!!!!vivek !!!!!>>>>>>>"
+print(b)
+print(b.rstrip("!"">")) #only for the tail elements
+print(b.replace("!","eee"))  #to replace any word or letter 
+# print(b.split(" "))     #to split the words separated b y space in a sentence or array
+print(a.capitalize())
+print(a.count("v")) # to count any word or letter in an array
+print(b.endswith(">"))    #true or false condition
+print(b.find("r"))    #finds the index , return -1 if not found
+# print(b.index("r"))    #return error if not found 
+print(b.isalnum())
+print(a.isalpha())
+print(b.islower())
+print(a.isupper())
+print(b.isprintable())
+print(b.isspace())
+print(a.istitle())
+print(a.startswith("v"))
+print(a.swapcase())      #upper to lower, lower to upper
+print(a.title())       #first letter is capital of each word
+
+

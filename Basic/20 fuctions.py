57372fdbd58ede98c6be296a35607122b57d6af0
VIVEK KUMAR <121549772+Vivekkumar121@users.noreply.github.com>
2023-07-06 00:04:50 +0530

    Add files via upload

---
+++ Basic/20 fuctions.py
@@ -0,0 +1,20 @@
+def factorial(x) :                                                       
+                                                                #factorial
+    a = 1
+    for i in range(1,x+1):
+        a = a * i
+    print("Your factorial is ",a)
+def isPrime(a) :
+                                                                #prime number
+    c = int(x/2)
+    for i in range(2,c+1) :
+        if(x % i == 0) :
+            return print("it is Not prime")
+    print("It is prime number")
+def fibonacci(x) : 
+    pass
+
+x = int(input("Enter the number  : "))
+# factorial(x)
+isPrime(x)
+
